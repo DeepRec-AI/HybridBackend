c9dcfe5f7b30221f2038787d52c2c466c537ae09
yuanman.ym <yuanman.ym@alibaba-inc.com>
2022-02-23 23:06:51 +0800

    [CI] Remove context and scope.

--- hybridbackend/tensorflow/__init__.py
+++ hybridbackend/tensorflow/__init__.py
@@ -24,8 +24,4 @@ from . import data
 from . import training as train
 
 from hybridbackend.libhybridbackend import buildinfo
-from hybridbackend.tensorflow.framework.context import Context
-from hybridbackend.tensorflow.framework.context import context
 from hybridbackend.tensorflow.framework.random import enable_deterministic
-from hybridbackend.tensorflow.framework.scope import function
-from hybridbackend.tensorflow.framework.scope import scope
